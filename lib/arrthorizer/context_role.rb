cd972fb8bca9f316602cd354c71ec9c931a74247
René van den Berg <r.van.den.berg@ogd.nl>
2013-10-04 20:55:07 +0200

    Make every ContextRole a Singleton
    This should be registered in the online guides: it is
    very important that ContextRoles are *never* given any
    state. We might want to suggest the State design pattern
    for such cases.

--- lib/arrthorizer/context_role.rb
+++ lib/arrthorizer/context_role.rb
@@ -1,7 +1,10 @@
 require 'arrthorizer/role'
+require 'singleton'
 
 module Arrthorizer
   class ContextRole < Role
+    include Singleton
+
     def name
       self.class.name.underscore
     end
--- spec/context_role/name_spec.rb
+++ spec/context_role/name_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Arrthorizer::ContextRole do
   describe :name do
     context "when the context role is not namespaced" do
-      let(:role) { UnnamespacedContextRole.new }
+      let(:role) { UnnamespacedContextRole.instance }
 
       it "returns a snake_cased version of the class name" do
         expect(role.name).to eql "unnamespaced_context_role"
@@ -11,7 +11,7 @@ describe Arrthorizer::ContextRole do
     end
 
     context "when the context role is namespaced" do
-      let(:role) { Namespaced::ContextRole.new }
+      let(:role) { Namespaced::ContextRole.instance }
 
       it "returns a snake_cased version of the class name" do
         expect(role.name).to eql "namespaced/context_role"
