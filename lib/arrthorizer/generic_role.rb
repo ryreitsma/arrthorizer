fa05a9136dd4826c3e645caa7bdc6b5e31968a97
Lennaert Meijvogel <lennaert.meijvogel@ogd.nl>
2013-12-17 10:57:22 +0100

    Add some documenting comments.

--- lib/arrthorizer/generic_role.rb
+++ lib/arrthorizer/generic_role.rb
@@ -1,5 +1,10 @@
 require 'arrthorizer/role'
 
+##
+# This is the class of all Generic Roles. A Generic Role is a role that does
+# not depend on context, but rather on membership - a user can be made a member
+# of such a role. This might be related to that person's role in the
+# organization, for example.
 module Arrthorizer
   class GenericRole < Role
   end
--- lib/arrthorizer/role.rb
+++ lib/arrthorizer/role.rb
@@ -1,3 +1,4 @@
+# This is the abstract superclass for all roles in the hierarchy.
 module Arrthorizer
   class Role
   end
