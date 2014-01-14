831e5b6fa3807eeb476da57dc522ba06d8289982
Rene van den Berg <rene.vandenberg@ogd.nl>
2014-01-14 17:19:06 +0100

    Solve a hairy inheritance-related issue in controller configurations

--- lib/arrthorizer/rails/controller_concern.rb
+++ lib/arrthorizer/rails/controller_concern.rb
@@ -4,7 +4,7 @@ module Arrthorizer
       extend ActiveSupport::Concern
 
       included do
-        delegate :arrthorizer_context_builder, to: 'self.class'
+        class_attribute :arrthorizer_context_builder, instance_writer: false
 
         ##
         # This is a hook method that provides access to the context for a
@@ -19,22 +19,13 @@ module Arrthorizer
       end
 
       module ClassMethods
-        # how does this work with inheritance again? does cattr_accessor help?
-        # possibly do something with inherited callback for parent class and
-        # set the instance variable on the child class. Then could we re-open
-        # the context-builder in subclasses without wrecking the inherited
-        # builder?
-        def arrthorizer_context_builder
-          @arrthorizer_context_builder
-        end
-
         ##
         # This is the configuration method for building Arrthorizer contexts from HTTP requests.
         # The developer specifies how contexts for authorization checks should be built
         # by providing a block to this method. For more information, check the documentation
         # on Arrthorizer::Rails::ControllerContextBuilder
         def to_prepare_context(&block)
-          @arrthorizer_context_builder = Arrthorizer::Rails::ControllerContextBuilder.new(&block)
+          self.arrthorizer_context_builder = Arrthorizer::Rails::ControllerContextBuilder.new(&block)
         end
       end
     end
--- spec/rails/controller_concern/to_prepare_context_spec.rb
+++ spec/rails/controller_concern/to_prepare_context_spec.rb
@@ -13,6 +13,20 @@ describe Arrthorizer::Rails::ControllerConcern do
       }.to change { controller_class.arrthorizer_context_builder }.to(expected_context_builder)
     end
 
+    context "when we are dealing with a subclassed controller" do
+      let(:controller_subclass) { Class.new(controller_class) }
+
+      before :each do
+        controller_class.to_prepare_context do end
+      end
+
+      it "does not alter the context builder for the superclass" do
+        expect {
+          controller_subclass.to_prepare_context do end
+        }.not_to change { controller_class.arrthorizer_context_builder }
+      end
+    end
+
     context "when no configuration block is provided" do
       specify "an Arrthorizer::Rails::ConfigurationError is raised" do
         expect {
