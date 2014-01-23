64f177ac6e303da759f41d0b3f991c7aa2486596
Rene van den Berg <rene.vandenberg@ogd.nl>
2014-01-23 18:37:03 +0100

    Add a Repository to Arrthorizer::Rails::ContextRole and add a .get_current() method

--- lib/arrthorizer/rails/controller_action.rb
+++ lib/arrthorizer/rails/controller_action.rb
@@ -5,19 +5,41 @@ module Arrthorizer
       ActionNotDefined = Class.new(Arrthorizer::ArrthorizerException)
 
       attr_accessor :privilege
-      attr_reader :controller, :action
+      attr_reader :controller_name, :action_name
+
+      def self.get_current(controller)
+        get_by_name(name_for(controller))
+      end
 
       def initialize(attrs)
-        self.controller = attrs.fetch(:controller) { raise ControllerNotDefined }
-        self.action = attrs.fetch(:action) { raise ActionNotDefined }
+        self.controller_name = attrs.fetch(:controller) { raise ControllerNotDefined }
+        self.action_name = attrs.fetch(:action) { raise ActionNotDefined }
+
+        self.class.register(self)
       end
 
       def name
-        [controller, action].join('#')
+        self.class.name_for(self)
       end
 
     private
-      attr_writer :controller, :action
+      attr_writer :controller_name, :action_name
+
+      def self.name_for(controller)
+        "#{controller.controller_name}##{controller.action_name}"
+      end
+
+      def self.get_by_name(name)
+        repository.get(name)
+      end
+
+      def self.register(controller_action)
+        repository.add(controller_action)
+      end
+
+      def self.repository
+        @repository ||= Repository.new
+      end
     end
   end
 end
