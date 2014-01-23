636c2a42def398b4404e7e6938b37c9e30eaa104
Rene van den Berg <rene.vandenberg@ogd.nl>
2014-01-23 18:14:22 +0100

    Add Arrthorizer::Rails::Configuration::Parser to turn config file into runtime config

--- lib/arrthorizer/rails/configuration.rb
+++ lib/arrthorizer/rails/configuration.rb
@@ -4,8 +4,7 @@ module Arrthorizer
       FileNotFound = Class.new(Arrthorizer::ArrthorizerException)
 
       def self.load
-        # Convert the configuration file contents to
-        # a runtime configuration.
+        Parser.new.process(config)
       end
 
     private
@@ -25,6 +24,39 @@ module Arrthorizer
 
       def self.config_file_name
         "config/arrthorizer.yml"
+      end
+
+      class Parser
+        def process(config)
+          config.each_pair do |privilege_name, configuration|
+            parse_privilege_node(privilege_name, OpenStruct.new(configuration))
+          end
+        end
+
+      private
+        def parse_privilege_node(name, description)
+          privilege = Arrthorizer::Privilege.new(name: name, roles: description.roles.map(&:constantize))
+
+          description.actions.each do |node|
+            controller_name = controller_from(node)
+
+            actions_from(node, controller_name).each do |action|
+              action.privilege = privilege
+            end
+          end
+        end
+
+        # node looks like this: { "controller_name" => ["namespaced/action_name", "another_action_name"] }
+        def controller_from(node)
+          node.keys.first
+        end
+
+        # node looks like this: { "controller_name" => ["namespaced/action_name", "another_action_name"] }
+        def actions_from(node, controller_name)
+          node[controller_name].map do |action_name|
+            Arrthorizer::Rails::ControllerAction.new(controller: controller_name, action: action_name)
+          end
+        end
       end
     end
   end
